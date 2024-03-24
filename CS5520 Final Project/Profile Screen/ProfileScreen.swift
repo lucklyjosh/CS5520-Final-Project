6695e5c85bc92ca45f9e6944b99bbe9146389c23
Chongjie Wen <lucklyjosh@gmail.com>
2024-03-24 00:14:08 -0400

    Josh

---
+++ CS5520 Final Project/Profile Screen/ProfileScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileScreen.swift
+//  CS5520 Final Project
+//
+//  Created by Josh wen on 3/24/24.
+//
+
+import UIKit
+
+class ProfileScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
