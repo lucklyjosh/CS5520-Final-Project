ea2b5953cb987b5ef83212c119a814c94a71d6ed
Chongjie Wen <lucklyjosh@gmail.com>
2024-03-23 21:32:36 -0400

    First

---
+++ CS5520 Final Project/Login/LoginScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LoginScreen.swift
+//  CS5520 Final Project
+//
+//  Created by Josh wen on 3/23/24.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
