9b301d1586e451f134a12bc64036bf96ec99e1e7
Zining <ziningxie10@gmail.com>
2024-04-12 11:35:09 -0400

    show-errors-when-clicking-save-button

---
+++ CS5520 Final Project/Data Model/Recipe.swift
@@ -0,0 +1,26 @@
+//
+//  Recipe.swift
+//  CS5520 Final Project
+//
+//  Created by zining xie on 4/12/24.
+//
+
+import Foundation
+import UIKit
+
+//MARK: struct to create a package to send to the Display Screen...
+struct Recipe {
+        var name:String?
+        var ingredients:String?
+        var instructions:String?
+        var image: UIImage?
+    
+        
+        init(name: String, ingredients: String,instructions: String ,image: UIImage) {
+            self.name = name
+            self.ingredients = ingredients
+            self.instructions = ingredients
+            self.image = image
+        }
+    }
+
